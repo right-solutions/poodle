d22161d8711cb918981ea5725ebf3efb94c42a27
Krishnaprasad Varma <kvarma@qwinixtech.com>
2015-02-05 10:05:49 +0530

    - Added kaminari sample era files for bootstrap look and feel - The parent application will have to manually add this to views folder

--- app/controllers/poodle/admin_controller.rb
+++ app/controllers/poodle/admin_controller.rb
@@ -1,146 +1,150 @@
-class Poodle::AdminController < PoodleController
+module Poodle
+  class AdminController < ApplicationController
 
-  layout 'poodle/application'
+    helper Poodle::ApplicationHelper
 
-  before_filter :require_admin, :set_navs, :parse_pagination_params
-  before_filter :configure
+    layout 'poodle/application'
 
-  def index
-    get_collections
-  end
+    before_filter :require_admin, :set_navs, :parse_pagination_params
+    before_filter :configure
 
-  def show
-    obj = @options[:class].find(params[:id])
-    instance_variable_set("@#{@options[:collection_name].to_s.singularize}", obj)
-    render_list
-  end
-
-  def new
-    obj = @options[:class].new
-    instance_variable_set("@#{@options[:collection_name].to_s.singularize}", obj)
-    render_list
-  end
-
-  def edit
-    obj = @options[:class].find(params[:id])
-    instance_variable_set("@#{@options[:collection_name].to_s.singularize}", obj)
-    render_list
-  end
-
-  def create
-    obj = @options[:class].new
-    obj.assign_attributes(permitted_params)
-    instance_variable_set("@#{@options[:collection_name].to_s.singularize}", obj)
-    save_resource(obj)
-  end
-
-  def update
-    obj = @options[:class].find(params[:id])
-    obj.assign_attributes(permitted_params)
-    instance_variable_set("@#{@options[:collection_name].to_s.singularize}", obj)
-    save_resource(obj)
-  end
-
-  def destroy
-    obj = @options[:class].find(params[:id])
-    obj.destroy
-    get_collections
-    set_flash_message(@options[:messages][:delete], :success)
-    respond_to do |format|
-      format.js { render :index }
+    def index
+      get_collections
     end
-  end
 
-  private
+    def show
+      obj = @options[:class].find(params[:id])
+      instance_variable_set("@#{@options[:collection_name].to_s.singularize}", obj)
+      render_list
+    end
 
-  def set_navs
-    set_nav(params[:controller])
-  end
+    def new
+      obj = @options[:class].new
+      instance_variable_set("@#{@options[:collection_name].to_s.singularize}", obj)
+      render_list
+    end
 
-  def permitted_params
-    raise "method 'permitted_params' not defined"
-  end
+    def edit
+      obj = @options[:class].find(params[:id])
+      instance_variable_set("@#{@options[:collection_name].to_s.singularize}", obj)
+      render_list
+    end
 
-  def default_collection_name
-     params[:controller].gsub("admin/", "").to_s
-  end
+    def create
+      obj = @options[:class].new
+      obj.assign_attributes(permitted_params)
+      instance_variable_set("@#{@options[:collection_name].to_s.singularize}", obj)
+      save_resource(obj)
+    end
 
-  def default_item_name
-    default_collection_name.singularize.gsub("_", " ").titleize
-  end
+    def update
+      obj = @options[:class].find(params[:id])
+      obj.assign_attributes(permitted_params)
+      instance_variable_set("@#{@options[:collection_name].to_s.singularize}", obj)
+      save_resource(obj)
+    end
 
-  def default_class
-    default_collection_name.singularize.camelize.constantize
-  end
+    def destroy
+      obj = @options[:class].find(params[:id])
+      obj.destroy
+      get_collections
+      set_flash_message(@options[:messages][:delete], :success)
+      respond_to do |format|
+        format.js { render :index }
+      end
+    end
 
-  def default_configuration
-    {
-      collection_name: default_collection_name,
-      item_name: default_item_name,
-      class: default_class,
-      messages: {
-        add: I18n.translate("forms.add", item: default_item_name),
-        create: I18n.translate("forms.create", item: default_item_name),
-        update: I18n.translate("forms.update", item: default_item_name),
-        save: I18n.translate("forms.save", item: default_item_name),
-        remove: I18n.translate("forms.remove", item: default_item_name),
-        delete: I18n.translate("forms.delete",item:  default_item_name)
+    private
+
+    def set_navs
+      set_nav(params[:controller])
+    end
+
+    def permitted_params
+      raise "method 'permitted_params' not defined"
+    end
+
+    def default_collection_name
+       params[:controller].gsub("admin/", "").to_s
+    end
+
+    def default_item_name
+      default_collection_name.singularize.gsub("_", " ").titleize
+    end
+
+    def default_class
+      default_collection_name.singularize.camelize.constantize
+    end
+
+    def default_configuration
+      {
+        collection_name: default_collection_name,
+        item_name: default_item_name,
+        class: default_class,
+        messages: {
+          add: I18n.translate("forms.add", item: default_item_name),
+          create: I18n.translate("forms.create", item: default_item_name),
+          update: I18n.translate("forms.update", item: default_item_name),
+          save: I18n.translate("forms.save", item: default_item_name),
+          remove: I18n.translate("forms.remove", item: default_item_name),
+          delete: I18n.translate("forms.delete",item:  default_item_name)
+        }
       }
-    }
-  end
-
-  def configure
-    if defined?(@options)
-      @options.reverse_merge!(default_configuration)
-    else
-      @options = default_configuration
     end
-  end
 
-  def prepare_query
-    @relation = @options[:class].where("")
-    if params[:query]
-      @query = params[:query].strip
-      @relation = @relation.search(@query) if !@query.blank?
+    def configure
+      if defined?(@options)
+        @options.reverse_merge!(default_configuration)
+      else
+        @options = default_configuration
+      end
     end
-  end
 
-  def get_collections
-    prepare_query
-    objects = @relation.order("created_at desc").page(@current_page).per(@per_page)
-    instance_variable_set("@#{@options[:collection_name]}", objects)
-    unless instance_variable_get("@#{@options[:collection_name].to_s.singularize}")
-      instance_variable_set("@#{@options[:collection_name].to_s.singularize}", objects.first)
+    def prepare_query
+      @relation = @options[:class].where("")
+      if params[:query]
+        @query = params[:query].strip
+        @relation = @relation.search(@query) if !@query.blank?
+      end
     end
-    return true
-  end
 
-  def save_resource(obj)
-    obj.save
-    set_flash_message(@options[:messages][:save], :success) if obj.errors.blank?
-    action_name = params[:action].to_s == "create" ? "new" : "edit"
-    url = obj.persisted? ? url_for([:admin, obj]) : nil
-    render_or_redirect(obj.errors.any?, url, action_name)
-  end
-
-  def render_or_redirect(error, redirect_url, action)
-    respond_to do |format|
-      format.html {
-        if error
-          render action: action
-        else
-          redirect_to redirect_url, notice: @options[:message]
-        end
-      }
-      format.js {}
+    def get_collections
+      prepare_query
+      objects = @relation.order("created_at desc").page(@current_page).per(@per_page)
+      instance_variable_set("@#{@options[:collection_name]}", objects)
+      unless instance_variable_get("@#{@options[:collection_name].to_s.singularize}")
+        instance_variable_set("@#{@options[:collection_name].to_s.singularize}", objects.first)
+      end
+      return true
     end
-  end
 
-  def render_list
-    respond_to do |format|
-      format.html { get_collections and render :index }
-      format.js {}
+    def save_resource(obj)
+      obj.save
+      set_flash_message(@options[:messages][:save], :success) if obj.errors.blank?
+      action_name = params[:action].to_s == "create" ? "new" : "edit"
+      url = obj.persisted? ? url_for([:admin, obj]) : nil
+      render_or_redirect(obj.errors.any?, url, action_name)
     end
-  end
 
+    def render_or_redirect(error, redirect_url, action)
+      respond_to do |format|
+        format.html {
+          if error
+            render action: action
+          else
+            redirect_to redirect_url, notice: @options[:message]
+          end
+        }
+        format.js {}
+      end
+    end
+
+    def render_list
+      respond_to do |format|
+        format.html { get_collections and render :index }
+        format.js {}
+      end
+    end
+
+  end
 end
--- app/controllers/poodle_controller.rb
+++
@@ -1,2 +0,0 @@
-class PoodleController < ApplicationController
-end

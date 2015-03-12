39ba71b0e48550e06b0b979be393116cca9ea180
Krishnaprasad Varma <kvarma@qwinixtech.com>
2015-03-12 13:24:16 +0530

    - Removing the storage configuration from image uploader   The application which uses poodle should be able to add a carrierwave.rb file in initialiser to configure storage and other details.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,12 +2,7 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
-  storage Rails.env.production? ? :fog : :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- lib/poodle/version.rb
+++ lib/poodle/version.rb
@@ -1,3 +1,3 @@
 module Poodle
-  VERSION = "0.1.6"
+  VERSION = "0.1.7"
 end
